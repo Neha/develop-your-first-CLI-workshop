3d9e0d477382bf181e103f04a53ad1e15ce25efd
neha <nsharma215@gmail.com>
2019-04-28 00:36:40 +0530

    Dummy data updated

--- session-2/myprofile/data/data.js
+++ session-2/myprofile/data/data.js
@@ -6,7 +6,7 @@ var data = {
   contactme:
     "You can email me at nsharma215@gmail.com\nTwitter : twitter.com/hellonehha\nLinkdin : linkdin.co./nehhasharma\n",
   experience:
-    "I have 8+ years of experience in front-end domain. Currently I am working as Technical Lead at HCL Products and Platforms. I am working with the team to develop and implement their new design system.",
+    "I have 8+ years of experience in front-end domain. Currently I am working as Technical Lead . I am working with the team to develop and implement their new design system.",
   techstack:
     "For front-end tooling  I am working on HTML5\nCSS3\nJavascript\nReactjs\nRedux\nNodejs. I know little bit of UI designing too and for that I use Photoshop and Sketch."
 };
